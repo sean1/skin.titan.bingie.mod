29d53644cec50f6e06d2ed0e4d598204af9669e1
Sean <email@domain.com>
2026-08-11 14:07:02 -0700

    Speed up detail shelf navigation

--- tests/test_window_animation_timing.py
+++ tests/test_window_animation_timing.py
@@ -14,6 +14,31 @@ class WindowAnimationTimingTests(unittest.TestCase):
 		effects = includes[0].findall("./animation[@type='WindowOpen']/effect[@type='fade']")
 		self.assertEqual([(effect.get('start'), effect.get('end'), effect.get('time')) for effect in effects], [('0', '100', '300')])
 
+	def test_visible_detail_shelves_use_300_millisecond_scrolls(self):
+		targets = {
+			'IncludesPovActor.xml': (610, 620, 630),
+			'IncludesPovInfo.xml': (550, 563, 560),
+			'IncludesDialogVideoInfo.xml': (550, 563, 560),
+		}
+		for filename, control_ids in targets.items():
+			root = ET.parse(ROOT / 'xml' / filename).getroot()
+			for control_id in control_ids:
+				with self.subTest(filename=filename, control_id=control_id):
+					panels = [control for control in root.iter('control') if control.get('type') == 'panel' and control.get('id') == str(control_id)]
+					self.assertEqual(len(panels), 1)
+					scrolltime = panels[0].find('scrolltime')
+					self.assertEqual((panels[0].findtext('orientation'), scrolltime.get('tween'), scrolltime.text), ('horizontal', 'quadratic', '300'))
+
+		root = ET.parse(ROOT / 'xml' / 'IncludesDialogVideoInfo.xml').getroot()
+		providers = [control for control in root.iter('control') if control.get('type') == 'panel' and control.get('id') == '50']
+		self.assertEqual(len(providers), 1)
+		provider = providers[0]
+		scrolltime = provider.find('scrolltime')
+		self.assertEqual(
+			(provider.findtext('left'), provider.findtext('top'), provider.findtext('width'), provider.findtext('height'), provider.findtext('orientation'), scrolltime.get('tween'), scrolltime.text),
+			('-2000', '-2000', '1', '1', 'horizontal', 'quadratic', '400')
+		)
+
 
 if __name__ == '__main__':
 	unittest.main()
